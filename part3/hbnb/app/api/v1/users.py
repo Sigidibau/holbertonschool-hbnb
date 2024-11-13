cbef10eb8404c826a240f7770169a92699014194
sigidibau <9571@holbertonstudents.com>
2024-11-13 12:51:25 -0400

    updated to get passwords

--- part3/hbnb/app/api/v1/users.py
+++ part3/hbnb/app/api/v1/users.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-
 from flask_restx import Namespace, Resource, fields
-from app.models import user
-from app.services import facade
+from hbnb.app.services import facade
 
 api = Namespace('users', description='User operations')
 
@@ -12,9 +10,8 @@ user_model = api.model('User', {
     'first_name': fields.String(required=True, description='First name of the user'),
     'last_name': fields.String(required=True, description='Last name of the user'),
     'email': fields.String(required=True, description='Email of the user'),
-    'is_admin': fields.Boolean(required=True, description='Whether the user is an admin')
+    'password': fields.String(required=True, description='Password of the user')
 })
-
 
 @api.route('/')
 class UserList(Resource):
@@ -36,9 +33,20 @@ class UserList(Resource):
                 'first_name': new_user.first_name,
                 'last_name': new_user.last_name,
                 'email': new_user.email,
-                'is_admin': new_user.is_admin}, 201
+        }, 201
 
+    @api.response(200, 'Get all users')
+    def get(self):
+        """Get all users"""
+        users = facade.get_all_users()
+        return [{
+            'id': user.id,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email
+        } for user in users], 200
 
+# User retrieval by ID
 @api.route('/<user_id>')
 class UserResource(Resource):
     @api.response(200, 'User details retrieved successfully')
@@ -48,33 +56,27 @@ class UserResource(Resource):
         user = facade.get_user(user_id)
         if not user:
             return {'error': 'User not found'}, 404
-        return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
+        return {'id': user.id,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'email': user.email
+                }, 200
 
-
-@api.route('/user-list')
-class Users(Resource):
-    @api.response(200, 'List of users retrieved successfully')
-    def get(self):
-        """List of users"""
-        users = facade.get_all_users()
-        return [{'id': user.id,
-                 'first_name': user.first_name,
-                 'last_name': user.last_name,
-                 'email': user.email} for user in users], 200
-
-
-@api.route('/update/<user_id>')
-class UserResource(Resource):
     @api.expect(user_model, validate=True)
-    @api.response(200, 'User updated successfully')
+    @api.response(200, 'User details updated successfully')
     @api.response(404, 'User not found')
     def put(self, user_id):
-        """Update user information"""
         user_data = api.payload
-        updated_user = facade.update_user(user_id, user_data)
-        if not updated_user:
+        user = facade.get_user(user_id)
+
+        if not user:
             return {'error': 'User not found'}, 404
-        return {'id': updated_user.id,
-                'first_name': updated_user.first_name,
-                'last_name': updated_user.last_name,
-                'email': updated_user.email}, 200
+
+        user.update(user_data)
+        return {
+            'id': user.id,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email,
+            'password': user.password
+        }, 200
