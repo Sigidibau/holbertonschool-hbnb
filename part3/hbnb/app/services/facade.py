0cee8489421cece935949d2011c8377b512a3d5f
sigidibau <9571@holbertonstudents.com>
2024-11-14 08:35:16 -0400

    all updates

--- part3/hbnb/app/services/facade.py
+++ part3/hbnb/app/services/facade.py
@@ -9,11 +9,20 @@ from app.persistence.repository import SQLAlchemyRepository
 
 class HBnBFacade:
     def __init__(self):
-        self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
-        self.user_repo = SQLAlchemyRepository(User)
+        self.user_repo = UserRepository()
+
+    def create_user(self, user_data):
+        user = User(**user_data)
+        user.hash_password(user_data['password'])
+        self.user_repo.add(user)
+        return user
+
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_user_by_email(email)
+
 
     # USER
     def create_user(self, user_data):
