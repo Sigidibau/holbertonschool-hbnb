e239973ebb0bbc28587fe2b9aefdd740727d3311
sigidibau <9571@holbertonstudents.com>
2024-11-17 21:34:09 -0400

    updated for db

--- part3/hbnb/app/models/reviews.py
+++ part3/hbnb/app/models/reviews.py
@@ -1,31 +1,36 @@
-from hbnb.app.models.basemodel import BaseModel
-from hbnb.app.models.places import Place
-from hbnb.app.models.users import User
-from hbnb.app import db, bcrypt
+from .basemodel import BaseModel
+from hbnb.app import db
+
 
 class Review(BaseModel):
-    tablename__ = 'reviews'
+    __tablename__ = 'review'
+    __table_args__ = {'extend_existing': True}
 
-first_name = db.Column(db.String(50), nullable=False)
-last_name = db.Column(db.String(50), nullable=False)
-email = db.Column(db.String(120), nullable=False, unique=True)
-password = db.Column(db.String(128), nullable=False)
-is_admin = db.Column(db.Boolean, default=False)
+    text = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.String(36), db.ForeignKey('user.id'), nullable=False)
+    place_id = db.Column(db.Integer, db.ForeignKey('place.id'), nullable=False)
 
+    def __init__(self, text, rating, place_id, user_id):
+        super().__init__()
+        self.text = text
+        self.rating = rating
+        self.place_id = place_id
+        self.user_id = user_id
+        self.validations()
 
+    def validations(self):
+        # Rating validation, must be between 0 and 5
+        if self.rating < 0 or self.rating > 5:
+            raise ValueError("Rating must be between 0 and 5")
 
-def validations(self):
-    # Rating validation, must be between 0 and 5
-    if self.rating < 0 or self.rating > 5:
-        raise ValueError("Rating must be between 0 and 5")
+        # Review text can't be empty
+        if self.text is None:
+            raise ValueError("Review required")
 
-    # Review text can't be empty
-    if self.text is None:
-        raise ValueError("Review required")
+        # Validate place and user ids to ensure they are properly assigned
+        if not isinstance(self.place_id, str) or not self.place_id:
+            raise ValueError("Place ID must be valid.")
 
-    # Validate place and user ids to ensure they are properly assigned
-    if not isinstance(self.place_id, str) or not self.place_id:
-        raise ValueError("Place ID must be valid.")
-
-    if not isinstance(self.user_id, str) or not self.user_id:
-        raise ValueError("User ID must be valid.")
+        if not isinstance(self.user_id, str) or not self.user_id:
+            raise ValueError("User ID must be valid.")
