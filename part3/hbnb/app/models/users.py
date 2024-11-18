5bed46d54daaac3bb6b11a0948d7f3b08440c408
sigidibau <9571@holbertonstudents.com>
2024-11-17 21:38:16 -0400

    updated

--- part3/hbnb/app/models/users.py
+++ part3/hbnb/app/models/users.py
@@ -1,49 +1,61 @@
 #!/usr/bin/python3
-
-from datetime import datetime
-from hbnb.app.models.basemodel import BaseModel
-from flask_bcrypt import Bcrypt, generate_password_hash, check_password_hash
+from .basemodel import BaseModel
+from hbnb.app import bcrypt, db
 import re
-from app import db, bcrypt
 import uuid
 
 
-# This variable is used to validate the email format
-regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
-
-flask_bcrypt = Bcrypt()
-
 class User(BaseModel):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
+    __table_args__ = {'extend_existing': True}
 
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(128), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
-    
-    def _validate_email(self, email):
-        if not re.fullmatch(regex, email):
-            raise ValueError("Invalid email format")
-        return email
 
-    def _validate_first_name(self, first_name):
-        if len(first_name) > 50:
-            raise ValueError("First name must be less than 50 characters")
-        if len(first_name) == 0:
-            raise ValueError("First name cannot be empty")
-        return first_name
-    def _validate_last_name(self, last_name):
-        if len(last_name) > 50:
-            raise ValueError("Last name must be less than 50 characters")
-        if len(last_name) == 0:
-            raise ValueError("Last name cannot be empty")
-        return last_name
+    def __init__(self, first_name, last_name, email, is_admin=False, password=None):
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.is_admin = is_admin
+        if password:
+            self.hash_password(password)
+
+        # Validations
+        self.validate_name()
+
+    def validate_name(self):
+        # First name length validation
+        if not self.first_name or len(self.first_name) > 50:
+            raise ValueError('First name must be less than 50 characters.')
+        # Last name length validation
+        if not self.last_name or len(self.last_name) > 50:
+            raise ValueError('Last name must be less than 50 characters.')
 
     def hash_password(self, password):
         """Hashes the password before storing it."""
-        self.password = flask_bcrypt.generate_password_hash(password).decode('utf-8')
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
 
     def verify_password(self, password):
         """Verifies if the provided password matches the hashed password."""
-        return flask_bcrypt.check_password_hash(self.password, password)
+        return bcrypt.check_password_hash(self.password, password)
+
+    def update(self, first_name: str = None, last_name: str = None, email: str = None, is_admin: bool = None):
+        if first_name:
+            self.first_name = first_name
+            self.validate_name()
+        if last_name:
+            self.last_name = last_name
+            self.validate_name()
+        if email:
+            self.email = email
+            self.validate_email()
+
+        if is_admin is not None:
+            self.is_admin = is_admin
+
+        # Update timestamp
+        self.save()
